bfe445f648b34e18958dd683e4e7548c73a27a2d
Mark Strendin <mark.strendin@gmail.com>
2021-01-28 21:37:23 -0600

    Improve logging and implement a blacklist. To avoid getting stuck in infinite loops from web calendars.

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -12,15 +12,15 @@ namespace LSSDEdlioFileArchiver.Scraper
 {
     class Program
     {
-        static string _edlioFilesDomain = "files.edl.io";
-        static string _outputFile_CrawledURls = "scraper-crawledurls.txt";
-        static string _outputFile_Downloadables = "scraper-downloadables.txt";
-        
-        // Don't put trailing slashes
-        static List<string> _rootURls = new List<string>() {
-            "https://www.lskysd.ca"
+        static readonly string _edlioFilesDomain = "files.edl.io";
+        static readonly string _outputFile_CrawledURls = $"scraper-crawledurls-{DateTime.Now.ToString("yyyy-MM-dd-HHmm")}.txt";
+        static readonly string _outputFile_Downloadables = $"scraper-downloadables-{DateTime.Now.ToString("yyyy-MM-dd-HHmm")}.txt";
+        static readonly string _outputFile_LogFile = $"scraper-log-{DateTime.Now.ToString("yyyy-MM-dd-HHmm")}.txt";
+        static readonly List<string> blacklisted_phrases = new List<string>() {
+            "/events/",
+            "subscribe/"
         };
-
+        
         static async Task<string> getHTMLBody(string url)
         {
             using (HttpClient httpClient = new HttpClient())
@@ -54,7 +54,6 @@ namespace LSSDEdlioFileArchiver.Scraper
             }
             catch {}
 
-            Console.WriteLine($"Found {foundLinks.Count} links.");
             return foundLinks;
         }
 
@@ -100,6 +99,15 @@ namespace LSSDEdlioFileArchiver.Scraper
                     continue;
                 }
 
+                // Ignore urls with blacklisted phrases
+                foreach(string blphrase in blacklisted_phrases) 
+                {
+                    if (massaged_url.Contains(blphrase, StringComparison.InvariantCultureIgnoreCase)) 
+                    {
+                        continue;
+                    }
+                }
+
                 // Convert relative URLs into absolute urls
                 if (massaged_url.StartsWith("/"))
                 {
@@ -115,7 +123,6 @@ namespace LSSDEdlioFileArchiver.Scraper
                 ok_urls.Add(massaged_url);
             }
 
-            Console.WriteLine($"Filtered {input_urls.Count} down to approved {ok_urls.Count} urls.");
             return ok_urls;
         }
 
@@ -129,76 +136,166 @@ namespace LSSDEdlioFileArchiver.Scraper
             }
         }
 
+        static string sanitizeRootURL(string inputString) 
+        {
+            string working = inputString;
+
+            if (string.IsNullOrEmpty(working)) {
+                return string.Empty;
+            }
+
+            if (!working.StartsWith("http://") || !working.StartsWith("https://")) 
+            {
+                working = $"https://{working}";
+            }
+
+            if (working.EndsWith("/"))
+            {
+                working.Remove(working.Length, 1);
+            }
+
+            return working;
+        }
+
+        static void Log(string msg) 
+        {
+            using(StreamWriter writer = File.AppendText(_outputFile_LogFile))
+            {
+                writer.WriteLine($"{DateTime.Now.ToString()}: {msg}");
+            }            
+        }
+
+        static void ConsoleWrite(string msg)
+        {
+            Log(msg);
+            Console.WriteLine($"{DateTime.Now.ToString("t")}: {msg}");
+        }
+
+        static void RecordVisitedURL(string filename, string url) 
+        {
+            using(StreamWriter writer = File.AppendText(filename))
+            {
+                writer.WriteLine(url);
+            }  
+
+        }
+
+        static void RecordDownloadable(string filename, string url) 
+        {
+            using(StreamWriter writer = File.AppendText(filename))
+            {
+                writer.WriteLine(url);
+            }  
+            
+        }
+
         static async Task Main(string[] args)
         {
+            Random random = new Random();
+
             List<string> _discoveredFiles = new List<string>();
             List<string> _visitedURLs = new List<string>();
             
-            foreach (string rooturl in _rootURls)
+            List<string> urlsToScan = new List<string>();
+            string filename_urls = _outputFile_CrawledURls;
+            string filename_downloadables = _outputFile_Downloadables;
+
+            // Get command line arguments
+            if (args.Length > 0) 
             {
-                Queue<string> urls_to_visit = new Queue<string>();
-                urls_to_visit.Enqueue(rooturl);
-
-                while(urls_to_visit.Count > 0)
+                // Until we find a better way, just accept the root url to scrape, or a list of root urls
+                foreach(string arg in args) 
                 {
-                    string this_url = urls_to_visit.Dequeue();
-
-                    Console.WriteLine($"Crawling {this_url}...");
-
-                    // Visit the url
-                    List<string> foundUrls = await getAllValidLinksFromURLAsync(this_url, rooturl);
-
-                    Console.WriteLine($"> Found {foundUrls.Count} urls");
-
-                    // Mark the url as visited
-                    _visitedURLs.Add(this_url);
-
-                    foreach(string url in foundUrls)
+                    if (!string.IsNullOrEmpty(arg)) 
                     {
-                        // Add files links to the discovered files collection
-                        if (url.Contains(_edlioFilesDomain)) {
-
-                            if (!_discoveredFiles.Contains(url))
+                        string sanitizedURL = sanitizeRootURL(arg);
+                        if (!string.IsNullOrEmpty(sanitizedURL))
+                        {
+                            if (!urlsToScan.Contains(sanitizedURL))
                             {
-                                Console.WriteLine($"Found file: {url}");
-                                _discoveredFiles.Add(url);
-                            }
-
-                        } else {
-
-                            // Add discovered links to the queue, as long as we've never been to them before
-                            if (!_visitedURLs.Contains(url))
-                            {
-                                if (!urls_to_visit.Contains(url))
-                                {
-                                    Console.WriteLine($"Adding site to queue: {url}");
-                                    urls_to_visit.Enqueue(url);
-                                }
+                                urlsToScan.Add(sanitizedURL);
                             }
                         }
                     }
-
-                    // Report our status
-                    Console.WriteLine($"Visited: {_visitedURLs.Count}, Left to visit: {urls_to_visit.Count}");
-
-                    // Wait a few second so we don't hammer the server
-                    Thread.Sleep(500);
-                }
-
-                Console.WriteLine("Crawl Complete.");
-                Console.WriteLine("Visited urls:");
-                foreach(string url in _visitedURLs)
-                {
-                    Console.WriteLine(url);
-                }
-                
-                Console.WriteLine("Found files:");
-                foreach(string url in _discoveredFiles)
-                {
-                    Console.WriteLine(url);
                 }
             }
-        
+
+            // Housekeeping
+            // If the output files already exist, remove them
+            if (File.Exists(filename_urls))
+            {
+                File.Delete(filename_urls);
+            }
+
+            if (File.Exists(filename_downloadables))
+            {
+                File.Delete(filename_downloadables);
+            }
+
+            if (urlsToScan.Count > 0)
+            {
+                // Start scrapin'
+                int rootCount = 0;
+                foreach (string rooturl in urlsToScan)
+                {
+                    rootCount++;
+                    Queue<string> urls_to_visit = new Queue<string>();
+                    urls_to_visit.Enqueue(rooturl);
+
+                    while(urls_to_visit.Count > 0)
+                    {
+                        string this_url = urls_to_visit.Dequeue();
+                        RecordVisitedURL(filename_urls, this_url);
+                        
+                        ConsoleWrite($"Crawling \"{this_url}\" ({rootCount}/{urls_to_visit.Count+1})...");
+
+                        // Visit the url
+                        List<string> foundUrls = await getAllValidLinksFromURLAsync(this_url, rooturl);
+
+                        // Mark the url as visited
+                        _visitedURLs.Add(this_url);
+
+                        foreach(string url in foundUrls)
+                        {
+                            // Add files links to the discovered files collection
+                            if (url.Contains(_edlioFilesDomain)) {
+
+                                if (!_discoveredFiles.Contains(url))
+                                {
+                                    Log($"> Found file: {url}");
+                                    _discoveredFiles.Add(url);
+                                    RecordDownloadable(filename_downloadables, url);
+                                }
+
+                            } else {
+
+                                // Add discovered links to the queue, as long as we've never been to them before
+                                if (!_visitedURLs.Contains(url))
+                                {
+                                    if (!urls_to_visit.Contains(url))
+                                    {
+                                        Log($"> Adding site to queue: {url}");
+                                        urls_to_visit.Enqueue(url);                                        
+                                    }
+                                }
+                            }
+                        }
+
+                        // Report our status
+                        Log($"Finished crawling {this_url}");
+
+                        // Wait a few second so we don't hammer the server
+                        Thread.Sleep(random.Next(100,500));
+                    }
+
+                    ConsoleWrite("Crawl Complete.");
+                    ConsoleWrite($"Visited {_visitedURLs.Count} urls.");
+                    ConsoleWrite($"Found {_discoveredFiles.Count} files.");
+
+                }  
+            } else {
+                ConsoleWrite("No urls to scrape!");
+            }     
         }
     }
 }
